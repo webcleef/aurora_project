c39676323324215913a038517621c689c52f8cbe
Andrey Merkushov <mercmero@mail.ru>
2017-04-24 18:57:20 +0300

    начал стилизовать контакты

--- gulpfile.js
+++ gulpfile.js
@@ -166,6 +166,7 @@ gulp.task('js', function () {
       dirs.source + '/js/jquery-3.1.0.min.js',
       dirs.source + '/js/jquery-migrate-1.4.1.min.js',
       dirs.source + '/js/owl.carousel.min.js',
+      dirs.source + '/js/slick.min.js',
       dirs.source + '/js/script.js',
     ])
     .pipe(plumber({ errorHandler: onError }))
